16ca985f45476a950cabd4faaf7cd30bba236461
Kamil Kubiak <kamiljakubkubiak@gmail.com>
2018-11-10 03:14:14 +0100

    abstracting ingame objects and and creating a skeleton for waypoint system

---
+++ AirportScanner/Assets/scripts/WaypointManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointManager : Singleton<WaypointManager> {
+
+    public List<Path> paths;
+
+
+}
+
