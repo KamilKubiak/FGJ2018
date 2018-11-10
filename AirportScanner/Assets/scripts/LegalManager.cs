dc86da9ab72076c882910482b6a8fc8540e13491
zbysz <suczynskizbigniew@gmail.com>
2018-11-10 03:17:13 +0100

    legalmanager enum

---
+++ AirportScanner/Assets/scripts/LegalManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Contraband
+{
+    Anaconda,
+    Applepie,
+    Banana,
+    BreakfastSausage,
+    CoolGlasses,
+    CDs,
+    DoubleBass,
+    Drumset,
+    Eggs,
+    EngineParts,
+    FurrySuit,
+    Flute,
+    Grenades,
+    Gremlins,
+    HarrisonFord,
+    HiFi,
+    Igloo,
+    InfinityStone,
+    JOCrystal,
+    JazzRecords,
+    Knives,
+    KnightArmor,
+    Lolipops,
+    Lego,
+    Money,
+    Macarel,
+    Nuts,
+    Nougat,
+    Oak,
+    OktoberfestBeer,
+    PokemonPlushie,
+    Pepperspray,
+    RedShirt,
+    Rump,
+    Stegosaurus,
+    Sextoys,
+    TeddyBear,
+    Tea,
+    UFO,
+    UniverseInACan,
+    VeterinaryBooks,
+    VeganCuisine,
+    WaterBottle,
+    Whisky,
+    Xylophone,
+    Xenomorph,
+    YetiFoot,
+    YellowSnow,
+    ZanzibarianCandies,
+    ZlotoIDiamenty
+}
+
+public class LegalManager : Singleton<LegalManager>
+{
+
+	
+}
