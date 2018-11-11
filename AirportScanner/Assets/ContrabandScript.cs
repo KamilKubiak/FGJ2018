aa88ea0b1b3477b6403cc9392f3be76d61c9d17a
Kamil Kubiak <kamiljakubkubiak@gmail.com>
2018-11-11 08:38:14 +0100

    improving object legibility

---
+++ AirportScanner/Assets/ContrabandScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContrabandScript : MonoBehaviour {
+
+    public float scale=1;
+
+    private void Start()
+    {
+        transform.localScale = Vector3.one * scale;
+    }
+}
